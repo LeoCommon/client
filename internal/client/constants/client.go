4216c87c086270844deca5d276d8224e1458506e
Eric Jedermann <jedermann@cs.uni-kl.de>
2025-02-13 12:06:25 +0100

    version update for LTS tests

--- internal/client/constants/client.go
+++ internal/client/constants/client.go
@@ -3,5 +3,5 @@ package constants
 const (
 	ClientServiceName    = "client.service"
 	RebootPendingTmpfile = "/tmp/.reboot-pending"
-	ClientServiceVersion = "1.0.6b"
+	ClientServiceVersion = "1.1.0"
 )
